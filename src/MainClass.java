0ab551ba35cd302f5c4d0f6791be20aab60f25a8
wakaadii <firefm2@gmail.com>
2023-10-06 14:50:18 +0300

    Homework week 1 test #2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,12 @@
 public class MainClass {
 
+    private int class_number = 4620;
 
     public int getLocalNumber() {
         return (14);
+    }
+    public int getClassNumber() {
+        return(this.class_number);
     }
 
 }
