75e1aea1942bba7ff8fb5f01cb33e6fe929b60ef
wakaadii <firefm2@gmail.com>
2023-10-06 15:14:31 +0300

    Homework week 1 test #3

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,11 +2,17 @@ public class MainClass {
 
     private int class_number = 4620;
 
+    private String class_string = "Hello, world";
+
     public int getLocalNumber() {
         return (14);
     }
     public int getClassNumber() {
         return(this.class_number);
+    }
+
+    public String getClassString() {
+        return (this.class_string);
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,5 +11,10 @@ public class MainClassTest extends MainClass{
     public void testGetClassNumber() {
         Assert.assertTrue("method getClassNumber is less, than 45", this.getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("method getClassString doesn't contains 'Hello' or 'hello' word", ((this.getClassString().contains("Hello")) | (this.getClassString().contains("hello"))));
+    }
 }
 
